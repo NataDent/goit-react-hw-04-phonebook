59ca642b632cc7e550cd508d1872bf9d4540f834
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-09-11 14:02:15 +0300

    Contacts are with a problem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,18 @@ import { Section } from './Section/Section';
 import { ContactList } from './ContactList/ContactList';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  //   const [contacts, setContacts] = useState([
+  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ]);
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) || []
+  );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    if (contacts !== null) setContacts(JSON.parse(contacts));
-  }, []);
-
-  useEffect(() => {
-    if (!contacts.length) return;
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
